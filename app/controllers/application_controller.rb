45a8e410318f7354fbc505f19b8e1fcc744c3baa
kaizokush <ushakya3@gmail.com>
2016-10-10 14:58:33 +0545

    Edit post working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:signup) { |u| u.permit(:email,:password,:password_confirmation,:name,:photo)}
     devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:email,:password,:remember_me)}
-    devise_parameter_sanitizer.permit(:account_update){|u| u.permit(:email,:password,:password_confirmation,:name,:photo)}
+    devise_parameter_sanitizer.permit(:account_update){|u| u.permit(:current_password,:email,:password,:password_confirmation,:name,:photo)}
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,13 +9,21 @@ class PostsController < ApplicationController
     end
   end
   def edit
-    @post=post.findd(params[:id])
+    @post=Post.find(params[:id])
   end
   def index
     @posts = Post.all.order('created_at DESC')
   end
   def new
     @post=Post.new
+  end
+  def update
+    @post=Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      redirect_to edit_post_path(params[:id])
+    end
   end
   private
   def post_params
