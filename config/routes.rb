a14d9a2225ab1f448b984bfe56f779bb2c459e18
kaizokush <ushakya3@gmail.com>
2016-10-15 09:55:06 +0545

    working around with CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # if user_signed_in? then
-  root 'posts#index'
-  # else
-  # root 'posts#new'
+  # devise_scope :user do
+  #   authenticated :user do
+  #     root 'posts#index', as: :authenticated_root
+  #   end
+  #
+  #   unauthenticated do
+  #     root 'devise/sessions#new', as: :unauthenticated_root
+  #   end
   # end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
   resources :posts do
     resources :comments
     resources :upvotes, only: :create
