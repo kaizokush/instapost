e967b66a4f8e7c28f15f0686441973f6b410d76d
kaizokush <ushakya3@gmail.com>
2016-10-26 15:55:01 +0545

    fixes to bug

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 gem 'acts_as_votable', '~> 0.10.0'
-gem 'font-awesome-sass', '~> 4.6.2'
+# gem 'font-awesome-sass', '~> 4.7.0'
 # create a form_for
 gem 'simple_form'
 # Uses carrierwave as photo uploading tool
--- app/controllers/downvotes_controller.rb
+++ app/controllers/downvotes_controller.rb
@@ -2,7 +2,7 @@ class DownvotesController < ApplicationController
   before_action :authenticate_user!
   def create
     @post = Post.find(params[:post_id])
-    @post.liked_by current_user
+    @post.downvote_from current_user
     redirect_to posts_path
     respond_to do |format|
       format.html {}
